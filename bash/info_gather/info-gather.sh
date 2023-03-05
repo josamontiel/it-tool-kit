a2add608d5e66f2c686d1c1216c0014c0f6810f1
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2023-03-05 16:59:21 +0000

    Add files via upload

---
+++ bash/info_gather/info-gather.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+: '
+This script will allow us to monitor any and all base level 
+analytics regarding the system. 
+'
+
+line="============================================================================================"
+
+echo "starting at $(date)"; echo $line
+
+echo "UPTIME"; uptime; echo $line
+
+echo "FREE"; df -h; echo $line
+
+echo "WHO"; who; echo $line
+
+echo "Finishing at $(date)"
