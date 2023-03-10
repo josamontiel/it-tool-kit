4d250a49a9bdfdddbbe0ee7f1159411912086190
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2023-03-10 09:18:08 +0000

    Update gen.py

--- python/pwgen/gen.py
+++ python/pwgen/gen.py
@@ -22,21 +22,41 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-###############################
-#   PASSWORD GENERATOR
-# Choose P/W length
-# Returns Generated P/W
-###############################
-
-import string
+import os
+import argparse
 import secrets
-# import argparse
-# from pathlib import Path
+import string
 
-def generate():
-    len = int(input("\nDesired Password length: "))
-    password = string.ascii_letters + string.digits + string.punctuation
-    gen = ''.join(secrets.choice(password) for i in range(len))
-    print(f"\n{gen}\n")
+def generate_pin(length):
+    """Generates a random PIN of specified length"""
+    return ''.join(secrets.choice(string.digits) for i in range(length))
 
-generate()
+def generate_password(length):
+    """Generates a random password of specified length"""
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(characters) for i in range(length))
+
+def generate_passphrase(num_words):
+    """Generates a random passphrase consisting of specified number of words"""
+    with open('/usr/share/dict/words') as f:
+        words = [word.strip() for word in f.readlines()]
+    passphrase = ' '.join(secrets.choice(words) for i in range(num_words))
+    return passphrase
+
+def main():
+    parser = argparse.ArgumentParser(description='Generates passcodes')
+    parser.add_argument('type', choices=['pin', 'password', 'passphrase'], help='type of passcode to generate')
+    parser.add_argument('-l', '--length', type=int, default=8, help='length of passcode (for PIN and password only)')
+    parser.add_argument('-w', '--num-words', type=int, default=4, help='number of words in passphrase (for passphrase only)')
+    args = parser.parse_args()
+
+    if args.type == 'pin':
+        print(generate_pin(args.length))
+    elif args.type == 'password':
+        print(generate_password(args.length))
+    elif args.type == 'passphrase':
+        print(generate_passphrase(args.num_words))
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    main()
